e6059d556e2147d8223aa2ef1c4e8d0adb7cc543
zhuwl120820 <zhuwl120820@gxwsxx.com>
2015-08-14 15:16:16 +0800

    配置源为本地文件读取调整，加入项目运行环境变量在读取的配置信息键之前。调整优化抛出异常的提示信息。

--- src/main/java/com/gxws/tool/link/properties/reader/FileReader.java
+++ src/main/java/com/gxws/tool/link/properties/reader/FileReader.java
@@ -2,6 +2,7 @@ package com.gxws.tool.link.properties.reader;
 
 import java.util.ResourceBundle;
 
+import com.gxws.tool.common.constant.ProjectConstant;
 import com.gxws.tool.link.properties.exception.LinkPropertiesBaseException;
 import com.gxws.tool.link.properties.exception.LinkPropertiesKeyException;
 import com.gxws.tool.link.properties.exception.LinkPropertiesReaderInitException;
@@ -15,47 +16,57 @@ import com.gxws.tool.link.properties.exception.LinkPropertiesValueException;
  */
 public class FileReader implements Reader {
 
-	private ResourceBundle linkFile = null;
+	private ResourceBundle linkFile;
 
 	// 文件名为link.properties
 	private static final String LINK_FILE_NAME = "link";
 
 	/**
+	 * 从本地文件配置源中获取配置信息
+	 * 
 	 * @author zhuwl120820@gxwsxx.com
 	 * @throws LinkPropertiesReaderInitException
-	 *             2015年3月12日上午11:55:37
-	 * 
+	 *             配置读取对象初始化异常
+	 * @since 1.0
 	 */
 	public FileReader() throws LinkPropertiesReaderInitException {
 		try {
 			linkFile = ResourceBundle.getBundle(LINK_FILE_NAME);
 		} catch (Exception e1) {
 			LinkPropertiesReaderInitException e = new LinkPropertiesReaderInitException();
+			e.appendMessage(e1.getMessage());
 			e.setStackTrace(e1.getStackTrace());
-			e.setMessage(":");
 			throw e;
 		}
 	}
 
+	/**
+	 * @see com.gxws.tool.link.properties.reader.Reader#valueString(java.lang.String)
+	 */
 	@Override
 	public String valueString(String propertyKey) throws LinkPropertiesBaseException {
+		ProjectConstant pc = ProjectConstant.instance();
 		if (propertyKey == null) {
 			LinkPropertiesKeyException e = new LinkPropertiesKeyException();
-			e.setMessage("属性名为null");
+			// e.setMessage(propertyKey + "属性名为null");
+			e.appendMessage("属性名为null");
 			throw e;
 		} else {
 			String k = propertyKey.trim();
-			if ("".equals(k)) {
+			if (k.isEmpty()) {
 				LinkPropertiesKeyException e = new LinkPropertiesKeyException();
-				e.setMessage("属性名不是null，但是值为空");
+				// e.setMessage(k + "属性名不是null，但是值为空");
+				e.appendMessage("属性名不是null，但是值为空");
 				throw e;
 			} else {
+				k = pc.getEnv() + "." + k;
 				try {
 					return linkFile.getString(k);
 				} catch (Exception e1) {
 					LinkPropertiesValueException e = new LinkPropertiesValueException();
 					e.setStackTrace(e1.getStackTrace());
-					e.setMessage("属性名 '" + k + "' 的值读取异常 , 检查值是否正确配置");
+					// e.setMessage("属性名 '" + k + "' 的值读取异常 , 检查值是否正确配置");
+					e.appendMessage("属性名：" + k);
 					throw e;
 				}
 			}
