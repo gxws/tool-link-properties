2a694e029fc8ab4114b4cef8b48924a8a54398d1
zhuwl120820 <zhuwl120820@gxwsxx.com>
2015-08-14 15:20:14 +0800

    添加了servlet context对象的指定名称（指定键）。

--- src/main/java/com/gxws/tool/link/properties/annotation/LinkProperties.java
+++ src/main/java/com/gxws/tool/link/properties/annotation/LinkProperties.java
@@ -21,12 +21,23 @@ import java.lang.annotation.Target;
 public @interface LinkProperties {
 
 	/**
-	 * 参数名
+	 * 指定link-properties参数配置源要读取值的参数名。<br>
+	 * 同时该参数名在spring properties中使用。<br>
 	 * 
-	 * @author zhuwl120820@gxwsxx.com 2015年2月10日上午11:29:51
-	 * 
+	 * @author zhuwl120820@gxwsxx.com
 	 * @return 参数名
+	 * @since 1.0
 	 */
 	public String value() default "";
 
+	/**
+	 * 在ServletContext对象中读取的键，即servletContext.getAttribute(attrName)中的attrName值。
+	 * <br>
+	 * 同时该键在jsp页面中使用el表达式获取ServletContext中的值，即${attrName}<br>
+	 * 
+	 * @author zhuwl120820@gxwsxx.com
+	 * @return ServletContext对象中读取的键
+	 * @since 1.2
+	 */
+	public String[]servletContextAttrNames() default "";
 }
